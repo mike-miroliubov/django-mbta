7a239e381d11c6aeef1470e74e5e141d9a3a5e1a
Mikhail Miroliubov <mike.miroliubov@gmail.com>
2024-01-10 22:54:19 -0500

    Docker compose

--- django_intro/settings.py
+++ django_intro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#hm$(_%@g4bn^4vv!dz6u12((1+0&ayl@(m%hg5#g!9kd12842'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default='django-insecure-#hm$(_%@g4bn^4vv!dz6u12((1+0&ayl@(m%hg5#g!9kd12842')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', default=False))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",") if os.environ.get("DJANGO_ALLOWED_HOSTS") else []
 
 # Application definition
 
